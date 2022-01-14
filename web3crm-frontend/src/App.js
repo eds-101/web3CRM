ff79156de208ff49a289eb9d1978d1bf2adf2b81
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-01-14 16:55:18 +0000

    add contacts moved into component

--- web3crm-frontend/src/App.js
+++ web3crm-frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { useState } from 'react'
 
 import ContactList from './components/ContactList/ContactList'
+import AddContact from './components/AddContact/AddContact';
 
 const DummyData = [{'name': 'Alice', 
 'company': 'Apple', 
@@ -14,49 +15,17 @@ const DummyData = [{'name': 'Alice',
 function App() { 
 
   const [Data, setData] = useState(DummyData)
-
-  function handleSubmit(e) {
-    e.preventDefault()
-    console.log(e.target[0].value)
-    // name={item['name']} company={item['company']} phone={item['phone']} email={item['email']} notes={item['notes']} contract={item['contract worth']}
-    const newContact = {
-      'name': e.target[0].value,
-      'company': e.target[1].value,
-      'phone': e.target[2].value,
-      'email': e.target[3].value,
-      'notes': e.target[4].value,
-      'contract': e.target[5].value
-    }
-    // update Data to push object
-    setData(prev => [...prev, newContact])
-
+  
+  function updateContacts(contact) {
+    setData(prev => [...prev, contact])
   }
 
   return (
     <div className="App">
       <ContactList data={Data}/>
-      <form
-        onSubmit={handleSubmit}>
-        <label> Name: <input type="text" name="name" /> </label> <br />
-        <label> Company: <input type="text" name="name" /> </label> <br />
-        <label> Phone: <input type="text" name="name" /> </label> <br />
-        <label> Email: <input type="text" name="name" /> </label> <br />
-        <label> Notes: <input type="text" name="name" /> </label> <br />
-        <label> Contract Worth: <input type="text" name="name" /> </label>
-        <input type="submit" value="Submit" />
-      </form>
-
+      <AddContact onFetchData={updateContacts}/>
     </div>
   );
 }
 
 export default App;
-
-//
-// Create fields to input the data
-
-// have the data update in the correct area of the program
-
-// ?validation
-
-// move to backend
---
+++ web3crm-frontend/src/components/AddContact/AddContact.js
@@ -0,0 +1,32 @@
+function AddContact(props) {
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        const newContact = {
+          'name': e.target[0].value,
+          'company': e.target[1].value,
+          'phone': e.target[2].value,
+          'email': e.target[3].value,
+          'notes': e.target[4].value,
+          'contract': e.target[5].value
+        }
+
+        props.onFetchData(newContact) 
+      }
+
+    return(
+        <form onSubmit={handleSubmit}>
+        <label> Name: <input type="text" name="name" /> </label> <br />
+        <label> Company: <input type="text" name="name" /> </label> <br />
+        <label> Phone: <input type="text" name="name" /> </label> <br />
+        <label> Email: <input type="text" name="name" /> </label> <br />
+        <label> Notes: <input type="text" name="name" /> </label> <br />
+        <label> Contract Worth: <input type="text" name="name" /> </label>
+        <input type="submit" value="Submit" />
+      </form>
+
+    )
+
+}
+
+export default AddContact
