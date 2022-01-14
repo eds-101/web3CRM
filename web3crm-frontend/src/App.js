aedfda2820d15b0463f8ae7ddb33b92d2a399ea9
Edeman George <71378676+eds-101@users.noreply.github.com>
2022-01-14 16:29:58 +0000

    add contact

--- web3crm-frontend/src/App.js
+++ web3crm-frontend/src/App.js
@@ -15,11 +15,48 @@ function App() {
 
   const [Data, setData] = useState(DummyData)
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(e.target[0].value)
+    // name={item['name']} company={item['company']} phone={item['phone']} email={item['email']} notes={item['notes']} contract={item['contract worth']}
+    const newContact = {
+      'name': e.target[0].value,
+      'company': e.target[1].value,
+      'phone': e.target[2].value,
+      'email': e.target[3].value,
+      'notes': e.target[4].value,
+      'contract': e.target[5].value
+    }
+    // update Data to push object
+    setData(prev => [...prev, newContact])
+
+  }
+
   return (
     <div className="App">
       <ContactList data={Data}/>
+      <form
+        onSubmit={handleSubmit}>
+        <label> Name: <input type="text" name="name" /> </label> <br />
+        <label> Company: <input type="text" name="name" /> </label> <br />
+        <label> Phone: <input type="text" name="name" /> </label> <br />
+        <label> Email: <input type="text" name="name" /> </label> <br />
+        <label> Notes: <input type="text" name="name" /> </label> <br />
+        <label> Contract Worth: <input type="text" name="name" /> </label>
+        <input type="submit" value="Submit" />
+      </form>
+
     </div>
   );
 }
 
 export default App;
+
+//
+// Create fields to input the data
+
+// have the data update in the correct area of the program
+
+// ?validation
+
+// move to backend
