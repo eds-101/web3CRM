f53ae1963ffa82bc83efab8b95f2170b06d11203
Benedict Lawrence <BTLawrence44@gmail.com>
2022-01-16 17:51:32 +0000

    Adding style to the form

--- web3crm-frontend/src/components/AddContact/AddContact.js
+++ web3crm-frontend/src/components/AddContact/AddContact.js
@@ -1,32 +1,74 @@
-function AddContact(props) {
+import './AddContact.css' 
+
+import React from 'react'
+
+import {useState} from 'react'
+
+function AddContact(props) { 
+
+    const [Name, setName] = useState() 
+    const [Email, setEmail] = useState()
+    const [PhoneNumber, setphoneNumber] = useState()
+    const [Company, setCompany] = useState()
+    const [Notes, setNotes] = useState()
+    const [ContractWorth, setContractWorth] = useState() 
+
+    function getName(event){  setName(event.target.value) } 
+    function getCompany(event) { setCompany(event.target.value) }  
+    function getPhoneNumber(event) { setphoneNumber(event.target.value) } 
+    function getEmail(event){ setEmail(event.target.value)} 
+    function getNotes(event){ setNotes(event.target.value)} 
+    function getContractWorth(event) {setContractWorth(event.target.value)}
+
+
 
     function handleSubmit(e) {
-        e.preventDefault()
-        const newContact = {
-          'name': e.target[0].value,
-          'company': e.target[1].value,
-          'phone': e.target[2].value,
-          'email': e.target[3].value, 
-          'notes': e.target[4].value,
-          'contractWorth': e.target[5].value
-        }  
-        console.log(newContact)
-        props.onFetchData(newContact) 
+        e.preventDefault() 
+        const newContact = { 'name': e.target[0].value, 'company': e.target[1].value, 'phone': e.target[2].value,
+          'email': e.target[3].value, 'notes': e.target[4].value,'contractWorth': e.target[5].value }  
+        props.onFetchData(newContact)   
+
+        setName('')  
+        setEmail('')
+        setNotes('')  
+        setphoneNumber('') 
+        setCompany('')
+        setContractWorth('') 
+        
       } 
 
       
 
-    return(
-        <form onSubmit={handleSubmit}>
-        <label> Name: <input type="text" name="name" /> </label> <br />
-        <label> Company: <input type="text" name="name" /> </label> <br />
-        <label> Phone: <input type="text" name="name" /> </label> <br />
-        <label> Email: <input type="text" name="name" /> </label> <br />
-        <label> Notes: <input type="text" name="name" /> </label> <br />
-        <label> Contract Worth: <input type="text" name="name" /> </label>
-        <input type="submit" value="Submit" />
-      </form>
+    return( 
+      <div className="formContaner">  
+         <form onSubmit={handleSubmit} className="form">   
+         <p className='formHeader'>Form</p>
+            <div className='formCon'>
 
+         <div className='form-item'>
+          <label> Name: </label>  
+          <input type="text" name="name" onChange={getName} value={Name}/> 
+          <label> Company: </label> 
+          <input type="text" name="comapny" onChange={getCompany} value={Company}/>
+         </div>
+
+          <div className='form-item'>
+          <label> Phone:</label>  
+          <input type="text" name="phone" onChange={getPhoneNumber} value={PhoneNumber}/> 
+          <label> Email: </label>  
+          <input type="text"  name="email" onChange={getEmail} value={Email}/> 
+          </div>
+
+          <div className='form-item'>
+            <label> Notes: </label> 
+            <input type="text" name="notes" onChange={getNotes} value={Notes}/>
+            <label> Contract Worth: </label>  
+            <input type="text"  name="ContractWorth" onChange={getContractWorth} value={ContractWorth}/> 
+          </div> 
+          </div> 
+            <input type="submit" value="Submit" className='submit'/> 
+        </form> 
+      </div>
     )
 
 }
